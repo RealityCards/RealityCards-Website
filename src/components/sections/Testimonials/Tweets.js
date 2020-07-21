005c8f2899478a27759241d2542fb873be43272f
vladmee <vladpresedinte@gmail.com>
2020-07-21 18:58:01 +0700

    Update tweets

--- src/components/sections/Testimonials/Tweets.js
+++ src/components/sections/Testimonials/Tweets.js
@@ -3,7 +3,7 @@ import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const Tweets = ({}) => {
   return (
-    <div className="d-flex flex-row justify-content-around">
+    <div className="d-flex flex-row flex-wrap justify-content-around">
       <TwitterTweetEmbed tweetId={"1284852231691546629"} />
       <TwitterTweetEmbed tweetId={"1276643346841247745"} />
       <TwitterTweetEmbed tweetId={"1285333285896245248"} />
