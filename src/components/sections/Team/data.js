4c3fbe77103f0c7d3deddfa95e2c9e71c1345882
vladmee <vladpresedinte@gmail.com>
2020-08-04 13:53:26 +0700

    Update team page

--- src/components/sections/Team/data.js
+++ src/components/sections/Team/data.js
@@ -2,7 +2,7 @@ export const team = {
   founders: [
     {
       image: require("./../../../assets/images/team/andrew.jpeg"),
-      name: "Andrew Stanger",
+      name: "Andrew",
       role: "Co-Founder and CEO",
       linkedin: "https://www.linkedin.com/in/andrewstanger1/",
       telegram: "https://t.me/andrewstanger",
@@ -10,15 +10,15 @@ export const team = {
     },
     {
       image: require("./../../../assets/images/team/vlad.jpg"),
-      name: "Vlad Micliuc",
+      name: "Vlad",
       role: "Co-Founder and CTO",
       linkedin: "https://www.linkedin.com/in/vladmicliuc/",
       telegram: "https://t.me/vladmee",
       mail: "vlad@realitycards.io",
     },
     {
-      image: require("./../../../assets/images/team/defidude.jpg"),
-      name: "DeFi Dude",
+      image: require("./../../../assets/images/team/hunter.jpg"),
+      name: "Hunter (DeFi Dude)",
       role: "Head of Community",
       twitter: "https://twitter.com/DeFiDude",
     },
@@ -26,7 +26,7 @@ export const team = {
   advisors: [
     {
       image: require("./../../../assets/images/team/patricio.jpeg"),
-      name: "Patricio Worthalter",
+      name: "Patricio",
       role:
         "Founder of <a href='https://www.poap.xyz/' class='no-style-href' target='_blank' rel='noopener noreferrer'>POAP Protocol</>",
       linkedin: "https://www.linkedin.com/in/worthalter/",
