4e809e4325d7e40fc1dd9c8dea5410b8792c5422
vladmee <vladpresedinte@gmail.com>
2020-08-31 13:23:37 +0700

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -xe
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
-  npm run build
+  CI=false npm run build
   rsync -rq --delete --rsync-path="mkdir -p website && rsync" \
   $TRAVIS_BUILD_DIR/public travis@134.122.107.221:website
 else
