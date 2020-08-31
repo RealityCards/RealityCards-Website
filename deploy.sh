82be884114849b6de8486ab8f67474dfec4d2ad6
vladmee <vladpresedinte@gmail.com>
2020-08-31 15:38:57 +0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
   CI=false npm run build
-  rsync -rq --delete --rsync-path="sudo mkdir -p website && rsync" \
+  rsync -rq --delete --rsync-path="mkdir -p website && rsync" \
   $TRAVIS_BUILD_DIR/build travis@134.122.107.221:../../var/www/sites
 else
   echo "Not deploying, since this branch isn't master."
