b20efddbf2103f93d1b0a3b0b8e45636540e1e7c
vladmee <vladpresedinte@gmail.com>
2020-09-16 21:46:56 +0700

    Add new tweet

--- src/components/sections/Testimonials/Tweets.js
+++ src/components/sections/Testimonials/Tweets.js
@@ -4,6 +4,7 @@ import { TwitterTweetEmbed } from "react-twitter-embed";
 const Tweets = ({}) => {
   return (
     <div className="d-flex flex-row flex-wrap justify-content-around">
+      <TwitterTweetEmbed tweetId={"1305629707745005570"} />
       <TwitterTweetEmbed tweetId={"1284852231691546629"} />
       <TwitterTweetEmbed tweetId={"1288360633281347586"} />
       <TwitterTweetEmbed tweetId={"1276643346841247745"} />
