41651d71d5caea61eed6f043076a513312d37f19
vladmee <vladpresedinte@gmail.com>
2020-09-22 09:39:19 +0700

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -xe
 
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-  eval "$(ssh-agent -s)"
-  ssh-add
-  CI=false npm run build
-  rsync -rq --delete --rsync-path="mkdir -p website && rsync" \
-  $TRAVIS_BUILD_DIR/build travis@134.122.107.221:../../var/www/sites/website
-  ssh travis@134.122.107.221 "pm2 restart" /var/www/configs/website.config.js
-else
-  echo "Not deploying, since this branch isn't master."
-fi
+eval "$(ssh-agent -s)"
+ssh-add
+CI=false npm run build
+rsync -rq --delete --rsync-path="mkdir -p website && rsync" \
+$TRAVIS_BUILD_DIR/build travis@165.232.46.140:../../var/www/sites/website
+ssh travis@165.232.46.140 "pm2 restart" /var/www/configs/website.config.js
