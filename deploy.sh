a15cbbe96cff977aca985a9e7ddfb5ffe8c64f34
Vlad Micliuc <vladpresedinte@gmail.com>
2021-01-14 11:28:49 +0700

    Change servers

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ eval "$(ssh-agent -s)"
 ssh-add
 CI=false npm run build
 rsync -rq --delete --rsync-path="mkdir -p website && rsync" \
-$TRAVIS_BUILD_DIR/build travis@165.232.46.140:../../var/www/sites/website
-ssh travis@165.232.46.140 "pm2 restart" /var/www/configs/website.config.js
+$TRAVIS_BUILD_DIR/build travis@104.248.166.92:../../var/www/sites/website
+ssh travis@104.248.166.92 "pm2 restart" /var/www/configs/website.config.js
