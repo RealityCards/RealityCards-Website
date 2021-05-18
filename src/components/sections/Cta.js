3d9de976a65d7e7e3dcfcdc62a9b0e174e26b002
PauliusKK <17347293+PauliusKK@users.noreply.github.com>
2021-05-19 01:58:07 +0300

    Another half got fixed

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
-import Input from '../elements/Input';
 
 const propTypes = {
   ...SectionProps.types,
@@ -42,82 +40,17 @@ const Cta = ({
     split && 'cta-split'
   );
 
-  const [email, setEmail] = useState('');
-  const [sent, setSent] = useState(false);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    axios
-      .post(`https://${process.env.REACT_APP_APP_URL}/api/newsletter`, {
-        email: email,
-      })
-      .then(function (response) {
-        setSent(true);
-        setLoading(false);
-      })
-      .catch(function (error) {
-        setError(true);
-        setLoading(false);
-      });
-  };
-
-  const handleChange = (e) => {
-    setError(false);
-    setEmail(e.target.value);
-  };
-
   return (
     <section {...props} className={outerClasses}>
       <div className="container">
         <div className={innerClasses}>
-          {!sent ? (
-            <>
-              <div className="cta-slogan">
-                <h3 className="m-0">Want to be kept in the loop?</h3>
-                {/* <p className="small text-white">
-                  Enter your email to be notified on new updates
-                </p> */}
-              </div>
-              <div className="cta-action">
-                <a
-                  href="https://realitycards.io/discord"
-                  className="button button-dark button-wide-mobile button-lg"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="discord"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                    height="16"
-                    style={{ marginRight: 10, marginLeft: 0 }}
-                  >
-                    <path
-                      fill="#ffffff"
-                      d="M297.216 243.2c0 15.616-11.52 28.416-26.112 28.416-14.336 0-26.112-12.8-26.112-28.416s11.52-28.416 26.112-28.416c14.592 0 26.112 12.8 26.112 28.416zm-119.552-28.416c-14.592 0-26.112 12.8-26.112 28.416s11.776 28.416 26.112 28.416c14.592 0 26.112-12.8 26.112-28.416.256-15.616-11.52-28.416-26.112-28.416zM448 52.736V512c-64.494-56.994-43.868-38.128-118.784-107.776l13.568 47.36H52.48C23.552 451.584 0 428.032 0 398.848V52.736C0 23.552 23.552 0 52.48 0h343.04C424.448 0 448 23.552 448 52.736zm-72.96 242.688c0-82.432-36.864-149.248-36.864-149.248-36.864-27.648-71.936-26.88-71.936-26.88l-3.584 4.096c43.52 13.312 63.744 32.512 63.744 32.512-60.811-33.329-132.244-33.335-191.232-7.424-9.472 4.352-15.104 7.424-15.104 7.424s21.248-20.224 67.328-33.536l-2.56-3.072s-35.072-.768-71.936 26.88c0 0-36.864 66.816-36.864 149.248 0 0 21.504 37.12 78.08 38.912 0 0 9.472-11.52 17.152-21.248-32.512-9.728-44.8-30.208-44.8-30.208 3.766 2.636 9.976 6.053 10.496 6.4 43.21 24.198 104.588 32.126 159.744 8.96 8.96-3.328 18.944-8.192 29.44-15.104 0 0-12.8 20.992-46.336 30.464 7.68 9.728 16.896 20.736 16.896 20.736 56.576-1.792 78.336-38.912 78.336-38.912z"
-                    ></path>
-                  </svg>{' '}
-                  Join on Discord
-                </a>
-              </div>
-            </>
-          ) : (
-            <div className="cta-slogan">
-              <h3 className={`m-0`}>Thank you for subscribing!</h3>
-              <p className="small text-white">
-                We'll keep you updated on our progress. Expect one email every
-                few weeks. We don't want to spam you!
-              </p>
-            </div>
-          )}
+          <div className="cta-slogan">
+            <h3 className={`m-0`}>Thank you for subscribing!</h3>
+            <p className="small text-white">
+              We'll keep you updated on our progress. Expect one email every
+              few weeks. We don't want to spam you!
+            </p>
+          </div>
         </div>
       </div>
     </section>
